b433136a0be97c39eee7bbcab8f93da0cb94809d
Laksh Lathiya <laksh.lathiya@sahayaak.com>
2025-06-27 20:06:35 +0530

    optimise imports

--- app/src/main/java/com/assignment/facescannerapp/data/ImageRepositoryImpl.kt
+++ app/src/main/java/com/assignment/facescannerapp/data/ImageRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package com.assignment.facescannerapp.data
 
 import android.content.Context
-import android.net.Uri
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
@@ -10,16 +9,12 @@ import com.assignment.facescannerapp.data.room.entity.FaceTag
 import com.assignment.facescannerapp.domain.model.FaceBox
 import com.assignment.facescannerapp.domain.model.FaceImage
 import com.assignment.facescannerapp.domain.repo_interface.ImageRepository
-import com.assignment.facescannerapp.helper.loadBitmapFromUri
 import com.assignment.facescannerapp.paging.FaceImagePagingSource
-import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetector
 import com.google.mlkit.vision.face.FaceDetectorOptions
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
-import kotlin.math.abs
 
 class ImageRepositoryImpl @Inject constructor(
     private val context: Context,
