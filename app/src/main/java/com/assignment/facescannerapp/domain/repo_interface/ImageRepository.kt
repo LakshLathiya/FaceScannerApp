3106eeb1b7f90529def28ad69befbf7c7bf3a406
Laksh Lathiya <laksh.lathiya@sahayaak.com>
2025-06-27 19:49:44 +0530

    optimise imports

--- app/src/main/java/com/assignment/facescannerapp/domain/repo_interface/ImageRepository.kt
+++ app/src/main/java/com/assignment/facescannerapp/domain/repo_interface/ImageRepository.kt
@@ -1,6 +1,5 @@
 package com.assignment.facescannerapp.domain.repo_interface
 
-import android.net.Uri
 import androidx.paging.PagingData
 import com.assignment.facescannerapp.domain.model.FaceBox
 import com.assignment.facescannerapp.domain.model.FaceImage
